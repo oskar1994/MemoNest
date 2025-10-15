ab9a0daaf8d523298d99c51d94eaaae7bde6e6f8
Oskar Strzelecki <oskar.strz@gmail.com>
2025-10-15 10:50:15 +0200

    calendar with basic functionality

--- fe/MemoNest/src/components/Calendar.tsx
+++ fe/MemoNest/src/components/Calendar.tsx
@@ -8,7 +8,14 @@ const Calendar = () => {
   const events: EventInput[] = [
     { title: 'Spotkanie z klientem', date: '2025-10-20' },
     { title: 'Demo projektu', date: '2025-10-22' },
-    { title: 'Rocznica', date: '2025-10-14 10:10' },
+    {
+      title: 'Rocznica',
+      date: '2025-10-14 10:10',
+      extendedProps: {
+        description: 'Druga rocznica ślubu z Moniką.',
+        location: 'Kraków, ul Blachnickiego 3',
+      },
+    },
   ];
 
   return (
@@ -25,6 +32,17 @@ const Calendar = () => {
           right: 'dayGridMonth,timeGridWeek,timeGridDay',
         }}
         events={events}
+        eventClick={(info) => {
+          info.jsEvent.preventDefault();
+          const { title, start, extendedProps } = info.event;
+
+          alert(
+            `Tytuł: ${title}\n` +
+              `Data: ${start?.toLocaleString()}\n` +
+              `Opis: ${extendedProps.description}\n` +
+              `Miejsce: ${extendedProps.location}`,
+          );
+        }}
       />
     </div>
   );
